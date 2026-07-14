2be37ba7d083e891f4bc9d5e1682058d978440fc
Corenlius Tuggerson <victor.semenov@gmail.com>
2026-07-14 12:44:49 -0400

    260709_02 removing some detailed logging

--- src/harness/manipulator/manipulator.py
+++ src/harness/manipulator/manipulator.py
@@ -56,7 +56,6 @@ class AbstractBenchmark(ABC):
 class SingleRowInsert(AbstractBenchmark):
 
     def benchmark(self) -> Dict[str, Any]:
-        logger.info(f"In SingleRowInsert benchmark w. {self.cat} and {self.schema}")
         stuff = f"QQPP{datetime.datetime.today().strftime('%Y%m%d_%H%M')}_{get_ascending_letters_within_minute()}"
         start = time.perf_counter()
         self.spark.sql(
@@ -65,9 +64,6 @@ class SingleRowInsert(AbstractBenchmark):
             """
         )
         duration = time.perf_counter() - start
-        logger.info(
-            f"DONE with SingleRowInsert benchmark w. {self.cat} and {self.schema} and {duration}"
-        )
         return {
             "total_seconds": duration,
         }
