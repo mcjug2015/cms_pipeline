028685ea627a4f35de91f233123f023b886d4198
Corenlius Tuggerson <victor.semenov@gmail.com>
2026-07-20 16:35:07 -0400

    260717_01 better date fmt

--- src/crutch_migrations/dml_migrations/20260720_2_all.sql
+++ src/crutch_migrations/dml_migrations/20260720_2_all.sql
@@ -1,5 +1,5 @@
 begin
 MERGE INTO {{cat}}.{{schema}}.metrics t1
 USING {{cat}}.{{schema}}.metrics t2 ON t1.id = t2.id
-WHEN MATCHED THEN UPDATE SET t1.metric_batch_id = concat('backfill-', date_format(t2.last_updated, 'YYYYMMDD_HH'));
+WHEN MATCHED THEN UPDATE SET t1.metric_batch_id = concat('backfill-', date_format(t2.last_updated, 'yMd_k'));
 end;
